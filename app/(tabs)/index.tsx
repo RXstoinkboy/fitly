90d661a23a54a745814aa58603bfa6179ece10d3
RXstoinkboy <erykludwin@gmail.com>
2025-09-07 22:16:11 +0200

    add option to change pictures

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,12 @@
-import { Button, Text, YStack, Image } from "@/components/ui";
+import { Button, Text, YStack, Image, Sheet } from "@/components/ui";
 import { useModelImage } from "@/hooks/model-image";
 import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 export default function HomeScreen() {
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => setOpen(true);
+
   const { modelImage, saveModelImage } = useModelImage();
 
   const pickImage = async () => {
@@ -34,34 +38,66 @@ export default function HomeScreen() {
   };
 
   return (
-    <YStack alignItems="center" justifyContent="center" height="100%">
-      {modelImage ? (
-        <YStack width={"100%"} flex={1} alignItems="center">
-          <Text>Here will be replace option</Text>
+    <>
+      <YStack alignItems="center" justifyContent="center" height="100%">
+        {modelImage ? (
+          <YStack width={"100%"} flex={1} alignItems="center">
+            <Button onPress={showDrawer}>Change picture</Button>
 
-          <Image
-            source={{ uri: modelImage, width: 200, height: 200 }}
-            width="100%"
-            height="auto"
-            aspectRatio={3 / 4}
-          />
-        </YStack>
-      ) : (
-        <YStack>
-          <Text>
-            No photo. Please choose your photo from gallery or take one with
-            your camera
-          </Text>
-          <YStack>
-            <Button onPress={pickImage} background={"$accent"}>
-              Choose photo
-            </Button>
-            <Button onPress={takeImage} background={"$accent"}>
-              Take photo
-            </Button>
+            <Image
+              source={{ uri: modelImage, width: 200, height: 200 }}
+              width="100%"
+              height="auto"
+              aspectRatio={3 / 4}
+            />
           </YStack>
-        </YStack>
-      )}
-    </YStack>
+        ) : (
+          <YStack>
+            <Text>
+              No photo. Please choose your photo from gallery or take one with
+              your camera
+            </Text>
+            <YStack>
+              <Button onPress={pickImage} background={"$accent"}>
+                Choose photo
+              </Button>
+              <Button onPress={takeImage} background={"$accent"}>
+                Take photo
+              </Button>
+            </YStack>
+          </YStack>
+        )}
+      </YStack>
+      <Sheet
+        forceRemoveScrollEnabled={open}
+        open={open}
+        onOpenChange={setOpen}
+        dismissOnSnapToBottom
+        zIndex={100_000}
+        animation="medium"
+      >
+        <Sheet.Overlay
+          animation="lazy"
+          backgroundColor="$shadow6"
+          enterStyle={{ opacity: 0 }}
+          exitStyle={{ opacity: 0 }}
+        />
+
+        <Sheet.Handle />
+        <Sheet.Frame
+          padding="$4"
+          justifyContent="center"
+          alignItems="center"
+          gap="$5"
+        >
+          <Button onPress={pickImage} background={"$accent"}>
+            Choose photo
+          </Button>
+          <Button onPress={takeImage} background={"$accent"}>
+            Take photo
+          </Button>
+        </Sheet.Frame>
+      </Sheet>
+    </>
   );
 }

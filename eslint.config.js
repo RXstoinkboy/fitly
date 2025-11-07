4983f1b0266d4ddaee7bab1342d65283c3969d48
RXstoinkboy <erykludwin@gmail.com>
2025-11-07 22:42:34 +0100

    setup prettier and eslint correctly

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettier = require('eslint-plugin-prettier');
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
-    plugins: ['prettier'],
+    ignores: ['dist/*', 'node_modules/*'],
+    plugins: { prettier },
     rules: {
       'prettier/prettier': 'error',
     },
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-    },
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    }
   },
 ]);

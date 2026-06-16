f21248edcf3d3de61d406d8ad1818bed534a5364
RXstoinkboy <erykludwin@gmail.com>
2026-06-16 23:16:40 +0200

    feat: new image loader when generating image

--- apps/mobile/components/v2/domain/generate-image-button.tsx
+++ apps/mobile/components/v2/domain/generate-image-button.tsx
@@ -2,21 +2,11 @@ import { Sparkles } from '@/icons';
 import { Button, Spinner } from '@/components/v2/ui';
 import { useGenerateImageMutation } from '@/queries/image-generation/mutation';
 import { useGeneratedImages, useModels, useSelectedGarments } from '@/state';
-import { useEffect, useState } from 'react';
 import { usePaywall } from '@/hooks';
 import { analyticsEvents, captureError, trackEvent } from '@/lib/analytics';
-
-const loadingStates = [
-  'Sending images...',
-  'Analyzing content...',
-  'Generating image...',
-  'Hold on...',
-  'Almost there...',
-];
+import { useLoadingState } from '@/hooks/use-loading-state';
 
 export const GenerateImageButton = () => {
-  const [loadingState, setLoadingState] = useState<number | null>(null);
-
   const { addGeneratedImage } = useGeneratedImages();
   const { currentModelId } = useModels();
   const selectedGarments = useSelectedGarments();
@@ -37,6 +27,8 @@ export const GenerateImageButton = () => {
       throw error;
     },
   });
+
+  const loadingState = useLoadingState({ isPending });
 
   const onGenerateImage = async () => {
     if (!currentModelId) {
@@ -79,35 +71,6 @@ export const GenerateImageButton = () => {
     selectedGarments.clearSelection();
   };
 
-  useEffect(() => {
-    let interval: ReturnType<typeof setInterval> | undefined;
-    let intervalTime = 3000;
-
-    if (isPending) {
-      setLoadingState(0);
-      interval = setInterval(() => {
-        setLoadingState((prev) => {
-          if (prev === null) {
-            return 0;
-          }
-
-          if (prev >= loadingStates.length - 1) {
-            return prev;
-          }
-
-          return prev + 1;
-        });
-      }, intervalTime);
-    } else {
-      setLoadingState(null);
-    }
-    return () => {
-      if (interval) {
-        clearInterval(interval);
-      }
-    };
-  }, [isPending]);
-
   return (
     <Button
       bg={'$accent1'}
@@ -116,7 +79,7 @@ export const GenerateImageButton = () => {
       disabled={isPending || isPresenting}
       icon={isPending ? Spinner : Sparkles}
       onPress={onGenerateImage}>
-      {isPending ? loadingStates[loadingState ?? 0] : 'Create'}
+      {isPending ? loadingState : 'Create'}
     </Button>
   );
 };
